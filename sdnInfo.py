23c8fddca26463ec35d946d875353f16730e5b33
Samanthabengi <103688596+Samanthabengi@users.noreply.github.com>
2022-05-03 17:15:10 -0400

    Add files via upload

--- sdnInfo.py
+++ sdnInfo.py
@@ -2,6 +2,7 @@
 import json
 import random
 import sys
+import re
 x=' '
 filename = sys.argv[1]
 with open(filename, 'r') as f:
@@ -11,6 +12,9 @@ with open(filename, 'r') as f:
     net2 = gData['graph']
     adminIP = sys.argv[2]
     adminBridge = sys.argv[3]
+    aip = re.match('\d{1,3}.\d{1,3}.\d{1,3}.', adminIP )
+    #print(aip.group(0))
+    
     # bridge info
     print("---")
     print("vars_bridge:")
@@ -37,7 +41,7 @@ with open(filename, 'r') as f:
         print(3*x + f"- {{hostname: \'{h['id']}\', vmip: \'{h['ip'][0]}\', vmBridge: \'{h['bridges'][0]}\', ip3: \'192.168.{str(random.randint(0, 255))}.{str(random.randint(0, 255))}\', switch: \'{switch}\'  }}") 
     for c in net['controllers'].values():
         print("vars_controller:")
-        print(3*x + f"- {{hostname: \'{c['id']}\', vmip: \'{adminIP}\', vmBridge: \'{adminBridge}\', ip3: \'192.168.{str(random.randint(0, 255))}.{str(random.randint(0, 255))}\', eth: '2'  }}")
+        print(3*x + f"- {{hostname: \'{c['id']}\', vmip: \'{aip.group(0)}2\', vmBridge: \'{adminBridge}\', ip3: \'192.168.{str(random.randint(0, 255))}.{str(random.randint(0, 255))}\', eth: '2'  }}")
         print(f"{c['id']}_vars:")
         print(3*x + f"- {{hostname: \'{c['id']}\', ip2: \'{c['ip'][0]}\', bridge2: \'{c['bridges'][0]}\', number: \'1\' }}")
     for s in net['routers'].values():
